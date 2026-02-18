2e39bda0287ed33148a3a07fe50fb7dd381d2154
odelllaureano4-droid <odelllaureano4@gmail.com>
2026-02-18 12:07:01 -0600

    Create Odell89.lua

---
+++ Odell89.lua
@@ -0,0 +1,8 @@
+-- korblox no support R6
+getgenv().Setting = {
+["Body"] = {
+["Korblox"] = true, -- false or true
+["Headless"] = true, -- false or true
+},
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/khen791/script-khen/refs/heads/main/KorbloxAndHeadless.txt", true))()
